7c34ef2887a30e12cb53ec5cb92ebf97c0ebd101
krylovis <kis.226@yandex.ru>
2025-03-31 16:34:43 +0300

    add stylelint

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -8,7 +8,7 @@ export default {
   testMatch: ['<rootDir>/src/**/*.test.{ts,tsx}'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
-    '^.+\\.(css|less|scss|sass|png|svg|jpg)$': 'babel-jest',
+    '^.+\\.(css|scss|png|svg|jpg)$': 'babel-jest',
   },
   globals: {
     __SERVER_PORT__: process.env.SERVER_PORT,
