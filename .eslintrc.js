49297d0effdb3818454c7c97c6537b0e2cbf9035
krylovis <kis.226@yandex.ru>
2025-03-31 21:57:07 +0300

    delete unusing

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,8 +8,6 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    // 'plugin:@typescript-eslint/recommended-type-checked',
-    // 'plugin:@typescript-eslint/stylistic-type-checked',
     'prettier',
     'airbnb',
   ],
