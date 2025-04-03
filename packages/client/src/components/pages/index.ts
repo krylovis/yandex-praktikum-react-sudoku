db1b0879c931e873b37549a441b176652d4e59fe
Mariia Safronova <mary.an.safronova@yandex.ru>
2025-04-04 00:50:51 +0300

    fix: fixed styles based on the review results

--- packages/client/src/components/pages/index.ts
+++ packages/client/src/components/pages/index.ts
@@ -1,7 +1,7 @@
 export { default as AuthPage } from './auth-page/AuthPage';
 export { default as ForumPage } from './forum-page/ForumPage';
 export { default as GamePage } from './game-page/GamePage';
-export { default as LeaderPage } from './LeaderPage/LeaderPage';
+export { default as LeaderPage } from './leader-page/LeaderPage';
 export { default as LoginPage } from './login-page/LoginPage';
 export { default as MainPage } from './main-page/MainPage';
 export { default as ProfilePage } from './profile-page/ProfilePage';
--- packages/client/src/components/pages/leader-page/LeaderPage.tsx
+++ packages/client/src/components/pages/leader-page/LeaderPage.tsx
@@ -1,3 +1,65 @@
+/* eslint-disable no-console */
+/* eslint-disable react/no-array-index-key */
+import { useState } from 'react';
+import style from './LeaderPage.module.scss';
+import avatar from '../../../assets/images/avatar.jpg';
+import leftBlueArrow from '../../../assets/icons/left_blue_arrow.svg';
+import leaders from './mock';
+import Pagination from '../../pagination/pagination';
+
 export default function LeaderPage() {
-  return <div className="leader-page">Страница лидерборда</div>;
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const rowsPerPage = 8; // Количество строк на странице
+  // Вычисляем общее количество страниц
+  const totalPages = Math.ceil(leaders.length / rowsPerPage);
+
+  // Определяем начальный и конечный индексы для текущей страницы
+  const startIndex = (currentPage - 1) * rowsPerPage;
+  const currentLeaders = leaders.slice(startIndex, startIndex + rowsPerPage);
+
+  // TODO: реализовать логику нажатия на кнопку назад
+  const handleBackBtnClick = () => {
+    console.log('The back button is clicked');
+  };
+
+  const headNames = ['Место', 'Аватар', 'Имя', 'Время'];
+
+  return (
+    <main className={style.leader_page}>
+      <div className={style.leader_page__wrap}>
+        <button className={style.leader_page__back_wrap} type="button" onClick={handleBackBtnClick}>
+          <img className={style.leader_page__back_icon} src={leftBlueArrow} alt="Стрелка назад" />
+          <p className={style.leader_page__back_text}>Назад</p>
+        </button>
+        <h1 className={style.leader_page__title}>Таблица лидеров</h1>
+
+        <div className={style.leader_page__table}>
+          <ul className={style.leader_page__raw}>
+            {headNames.map((headName, ind) => (
+              <li className={`${style.leader_page__head_text} ${style.leader_page__text} ${headName === 'Имя' ? style.leader_page__name : ''}`} key={ind}>{headName}</li>
+            ))}
+          </ul>
+          <ul className={style.leader_page__table_content}>
+            {currentLeaders.map((leader, index) => (
+              <li className={style.leader_page__raw} key={index}>
+                <p className={`${style.leader_page__raw_text} ${style.leader_page__text}`}>{leader.place}</p>
+                <div className={style.leader_page__avatar_wrap}>
+                  <img className={style.leader_page__avatar} src={avatar} alt="Аватар" />
+                </div>
+                <p className={`${style.leader_page__raw_text} ${style.leader_page__text} ${style.leader_page__name}`}>{leader.name}</p>
+                <p className={`${style.leader_page__raw_text} ${style.leader_page__text} ${style.leader_page__last_child}`}>{leader.time}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={setCurrentPage}
+        />
+      </div>
+    </main>
+  );
 }
