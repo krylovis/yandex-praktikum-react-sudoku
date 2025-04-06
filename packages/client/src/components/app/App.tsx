2e5699bb228e91f3c17d29ea56ad9e2d51a1148b
krylovis <kis.226@yandex.ru>
2025-04-06 16:30:14 +0300

    delete unusing

--- packages/client/src/components/app/App.tsx
+++ packages/client/src/components/app/App.tsx
@@ -1,5 +1,5 @@
-import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { memo } from 'react';
 import {
   AuthPage,
   ForumPage,
@@ -11,61 +11,20 @@ import {
   TopicPage,
 } from '../pages/index';
 import ROUTES from '../../constants/constants';
-import Navigation from '../navigation/NavigationComponent';
-
-// eslint-disable-next-line object-curly-newline
-const hideNavigationOnRoutes: string[] = [
-  ROUTES.SIGN_UP,
-  ROUTES.FORUM,
-  ROUTES.GAME,
-  ROUTES.LEADERBOARD,
-  ROUTES.LOGIN,
-  ROUTES.PROFILE,
-  ROUTES.TOPIC,
-  ROUTES.MAIN,
-];
 
 function App() {
-  const navigate = useNavigate();
-  const location = useLocation();
-  useEffect(() => {
-    const fetchServerData = async () => {
-      // eslint-disable-next-line no-undef
-      const url = `http://localhost:${__SERVER_PORT__}`;
-      const response = await fetch(url);
-      const data = await response.json();
-      // eslint-disable-next-line no-console
-      console.log(data);
-    };
-
-    fetchServerData();
-  }, []);
   return (
-    <>
-      <Routes>
-        <Route path={ROUTES.SIGN_UP} element={<AuthPage />} />
-        <Route path={ROUTES.FORUM} element={<ForumPage />} />
-        <Route path={ROUTES.GAME} element={<GamePage />} />
-        <Route path={ROUTES.LEADERBOARD} element={<LeaderPage />} />
-        <Route path={ROUTES.LOGIN} element={<LoginPage />} />
-        <Route path={ROUTES.MAIN} element={<MainPage />} />
-        <Route path={ROUTES.PROFILE} element={<ProfilePage />} />
-        <Route path={ROUTES.TOPIC} element={<TopicPage />} />
-      </Routes>
-      {!hideNavigationOnRoutes.includes(location.pathname) && (
-        <div>Вот тут будет жить ваше приложение :)</div>
-      )}
-      {!hideNavigationOnRoutes.includes(location.pathname) && <Navigation />}
-      {/* Кнопка "Назад" для определённых страниц */}
-      {hideNavigationOnRoutes.includes(location.pathname) && (
-        <div>
-          <button type="button" onClick={() => navigate('/')}>
-            Назад
-          </button>
-        </div>
-      )}
-    </>
+    <Routes>
+      <Route path={ROUTES.SIGN_UP} element={<AuthPage />} />
+      <Route path={ROUTES.FORUM} element={<ForumPage />} />
+      <Route path={ROUTES.GAME} element={<GamePage />} />
+      <Route path={ROUTES.LEADERBOARD} element={<LeaderPage />} />
+      <Route path={ROUTES.LOGIN} element={<LoginPage />} />
+      <Route path={ROUTES.MAIN} element={<MainPage />} />
+      <Route path={ROUTES.PROFILE} element={<ProfilePage />} />
+      <Route path={ROUTES.TOPIC} element={<TopicPage />} />
+    </Routes>
   );
 }
 
-export default App;
+export default memo(App);
