63dc119e8c42f5caa2813e325d515d2e145d2549
krylovis <kis.226@yandex.ru>
2025-04-07 08:55:40 +0300

    return navigation

--- packages/client/src/components/app/App.tsx
+++ packages/client/src/components/app/App.tsx
@@ -1,5 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
-import { memo } from 'react';
+import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
 import {
   AuthPage,
   ForumPage,
@@ -11,20 +10,48 @@ import {
   TopicPage,
 } from '../pages/index';
 import ROUTES from '../../constants/constants';
+import Navigation from '../navigation/NavigationComponent';
+
+const hideNavigationOnRoutes: string[] = [
+  ROUTES.SIGN_UP,
+  ROUTES.FORUM,
+  ROUTES.GAME,
+  ROUTES.LEADERBOARD,
+  ROUTES.LOGIN,
+  ROUTES.PROFILE,
+  ROUTES.TOPIC,
+  ROUTES.MAIN,
+];
 
 function App() {
+  const navigate = useNavigate();
+  const location = useLocation();
   return (
-    <Routes>
-      <Route path={ROUTES.SIGN_UP} element={<AuthPage />} />
-      <Route path={ROUTES.FORUM} element={<ForumPage />} />
-      <Route path={ROUTES.GAME} element={<GamePage />} />
-      <Route path={ROUTES.LEADERBOARD} element={<LeaderPage />} />
-      <Route path={ROUTES.LOGIN} element={<LoginPage />} />
-      <Route path={ROUTES.MAIN} element={<MainPage />} />
-      <Route path={ROUTES.PROFILE} element={<ProfilePage />} />
-      <Route path={ROUTES.TOPIC} element={<TopicPage />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path={ROUTES.SIGN_UP} element={<AuthPage />} />
+        <Route path={ROUTES.FORUM} element={<ForumPage />} />
+        <Route path={ROUTES.GAME} element={<GamePage />} />
+        <Route path={ROUTES.LEADERBOARD} element={<LeaderPage />} />
+        <Route path={ROUTES.LOGIN} element={<LoginPage />} />
+        <Route path={ROUTES.MAIN} element={<MainPage />} />
+        <Route path={ROUTES.PROFILE} element={<ProfilePage />} />
+        <Route path={ROUTES.TOPIC} element={<TopicPage />} />
+      </Routes>
+      {!hideNavigationOnRoutes.includes(location.pathname) && (
+        <div>Вот тут будет жить ваше приложение :)</div>
+      )}
+      {!hideNavigationOnRoutes.includes(location.pathname) && <Navigation />}
+      {/* Кнопка "Назад" для определённых страниц */}
+      {hideNavigationOnRoutes.includes(location.pathname) && (
+        <div>
+          <button type="button" onClick={() => navigate('/')}>
+            Назад
+          </button>
+        </div>
+      )}
+    </>
   );
 }
 
-export default memo(App);
+export default App;
