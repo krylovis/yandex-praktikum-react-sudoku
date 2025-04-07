73f9017591fa38a75c8d3c5717f5197ee7619117
Eugeniya Volegzhanina <volegzhaninaem@yandex.ru>
2025-04-08 01:22:03 +0700

    Сверстана страница создания топика

--- packages/client/src/components/pages/topic-create-page/TopicCreatePage.tsx
+++ packages/client/src/components/pages/topic-create-page/TopicCreatePage.tsx
@@ -1,34 +1,54 @@
 import { FormEvent, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import styles from './TopicCreatePage.module.scss';
+import BackButton from '../../back-button/BackButton';
+import CustomButton from '../../custom-button/CustomButton';
 
 export default function TopicCreatePage() {
-  const navigate = useNavigate();
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
+  const { page, createTopicForm, formGroup, saveButton } = styles;
 
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
     console.log('Created topic:', { title, content });
-    navigate('/');
+    setTitle('');
+    setContent('');
   };
   return (
-    <div className="forum-page">
-      <h1>Create a New Topic</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Title"
-          value={title}
-          onChange={(event) => setTitle(event.target.value)}
-          required
-        />
-        <textarea
-          placeholder="Content"
-          value={content}
-          onChange={(event) => setContent(event.target.value)}
-          required
-        />
-        <button type="submit">Create</button>
+    <div className={page}>
+      <h1>Создать новую тему</h1>
+      <BackButton />
+      <form className={createTopicForm} onSubmit={handleSubmit}>
+        <div className={formGroup}>
+          <label htmlFor="title">Заголовок</label>
+          <input
+            id="title"
+            type="text"
+            placeholder="Введите заголовок топика"
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
+            required
+          />
+        </div>
+
+        <div className={formGroup}>
+          <label htmlFor="content">Содержимое</label>
+          <textarea
+            id="content"
+            placeholder="Введите содержимое топика"
+            value={content}
+            onChange={(event) => setContent(event.target.value)}
+            required
+          />
+        </div>
+        <div className={saveButton}>
+          <CustomButton
+            type="submit"
+            color="primary"
+            text="Создать"
+            onClick={handleSubmit}
+          />
+        </div>
       </form>
     </div>
   );
