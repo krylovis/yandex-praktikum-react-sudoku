e906aab4195e53880e971f6049186fddeb221e1f
krylovis <kis.226@yandex.ru>
2025-04-08 16:41:29 +0300

    add SignupPage

--- packages/client/src/components/pages/signup-page/SignupPage.tsx
+++ packages/client/src/components/pages/signup-page/SignupPage.tsx
@@ -1,9 +1,106 @@
-import ErrorBoundary from '../../utils';
+import { FormEvent, useCallback, memo } from 'react';
+import { useNavigate } from 'react-router-dom';
+import {
+  ContentContainer, FormField, MainForm,
+} from '../../index';
+import ROUTES from '../../../constants/constants';
+import useForm from '../../utils/hooks/useForm';
 
-export default function SignupPage() {
+// Регистрация с формой, имена полей: first_name, second_name, login, email, password, phone.
+interface IProps {
+  id: string,
+  placeholder: string,
+  type: 'number' | 'email' | 'text' | 'password' | 'tel',
+}
+
+const formText = {
+  formTitle: 'Регистрация',
+  submitText: 'Зарегистрироваться',
+  linkText: 'Войти',
+};
+
+const loginInputs: IProps[] = [
+  {
+    id: 'first_name',
+    placeholder: 'Введите имя',
+    type: 'text',
+  },
+  {
+    id: 'second_name',
+    placeholder: 'Введите фамилию',
+    type: 'text',
+  },
+  {
+    id: 'email',
+    placeholder: 'Введите почту',
+    type: 'email',
+  },
+  {
+    id: 'phone',
+    placeholder: 'Введите телефон',
+    type: 'tel',
+  },
+  {
+    id: 'login',
+    placeholder: 'Придумайте логин',
+    type: 'text',
+  },
+  {
+    id: 'password',
+    placeholder: 'Введите пароль',
+    type: 'password',
+  },
+  {
+    id: 'password_confirmation',
+    placeholder: 'Повторите пароль',
+    type: 'password',
+  },
+];
+
+function SignupPage() {
+  const { values, handleChange } = useForm({
+    first_name: '',
+    second_name: '',
+    email: '',
+    phone: '',
+    login: '',
+    password: '',
+    password_confirmation: '',
+  });
+  const navigate = useNavigate();
+
+  const handleNavigate = useCallback(() => {
+    navigate(ROUTES.LOGIN);
+  }, []);
+
+  const handleSubmit = useCallback((e: FormEvent) => {
+    e.preventDefault();
+    console.log('handleSubmit', values);
+  }, [values]);
+
   return (
-    <div className="signup-page">
-      <ErrorBoundary>Страница регистрации</ErrorBoundary>
-    </div>
+    <ContentContainer>
+      <MainForm
+        formTitle={formText.formTitle}
+        submitText={formText.submitText}
+        linkText={formText.linkText}
+        onSubmit={handleSubmit}
+        onNavigate={handleNavigate}
+      >
+
+        {loginInputs.map(({ id, type, placeholder }) => (
+          <FormField
+            key={id}
+            id={id}
+            type={type}
+            placeholder={placeholder}
+            value={values[id]}
+            onChange={handleChange}
+          />
+        ))}
+      </MainForm>
+    </ContentContainer>
   );
 }
+
+export default memo(SignupPage);
