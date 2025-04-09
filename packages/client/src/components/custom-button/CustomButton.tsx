2d19959677d72656b7b82157bde56466845d3964
nikitayakovina <36497484+nikitayakovina@users.noreply.github.com>
2025-04-09 22:03:45 +0600

    TASK-3: + main page

--- packages/client/src/components/custom-button/CustomButton.tsx
+++ packages/client/src/components/custom-button/CustomButton.tsx
@@ -1,21 +1,26 @@
 import { FormEvent, memo } from 'react';
+import classNames from 'classnames';
 import style from './CustomButton.module.scss';
 
 interface IProps {
   type: 'submit' | 'reset' | 'button',
-  color: 'primary' | 'secondary' | 'succes' | 'transparent',
+  color: 'primary' | 'secondary' | 'success' | 'transparent',
   text?: string,
+  className?: string[],
   title?: string,
   onClick: (e: FormEvent) => void,
 }
 
-function CustomButton({ text = '', title = '', type, color, onClick }: IProps) {
-  const className = [style.customButton];
-  className.push(style[`customButton_${color}`]);
+function CustomButton({ text = '', title = '', type, color, onClick, className = [] }: IProps) {
+  const classList = classNames(
+    ...className,
+    style.customButton,
+    style[`customButton_${color}`]
+  );
 
   return (
     <button
-      className={className.join(' ')}
+      className={classList}
       type={type}
       title={title}
       onClick={onClick}
--- packages/client/src/components/pages/main-page/MainPage.tsx
+++ packages/client/src/components/pages/main-page/MainPage.tsx
@@ -1,9 +1,62 @@
+import { useNavigate } from 'react-router-dom';
 import ErrorBoundary from '../../utils';
+import style from './MainPage.module.scss';
+import ROUTES from '../../../constants/constants';
+import CustomButton from '../../custom-button/CustomButton';
+
+type Action = {
+  text: string,
+  onNavigate: () => void,
+  style?: string,
+};
 
 export default function MainPage() {
+  const navigate = useNavigate();
+
+  const actions: Action[] = [
+    {
+      text: 'Продолжить',
+      onNavigate: () => navigate(ROUTES.GAME),
+    },
+    {
+      text: 'Новая игра',
+      onNavigate: () => navigate(ROUTES.GAME),
+    },
+    {
+      text: 'Личный кабинет',
+      onNavigate: () => navigate(ROUTES.PROFILE),
+    },
+    {
+      text: 'Таблица лидеров',
+      onNavigate: () => navigate(ROUTES.LEADERBOARD),
+    },
+    {
+      text: 'Форум',
+      onNavigate: () => navigate(ROUTES.FORUM),
+    },
+  ];
+
   return (
-    <div className="main-page">
-      <ErrorBoundary>Главная страница</ErrorBoundary>
-    </div>
+    <section className={style.mainPage}>
+      <ErrorBoundary>
+        <span className={style.mainPage__title}>
+          SUDOKU
+        </span>
+        <div className={style.mainPage__resultContainer}>
+          <img src="src/assets/images/mainPage.png" alt="Result game" className={style.mainPage__result} />
+        </div>
+        <div className={style.mainPage__actions}>
+          {actions.map(({ text, onNavigate }) => (
+            <CustomButton
+              className={[style.button]}
+              type="button"
+              color="secondary"
+              text={text}
+              onClick={onNavigate}
+            />
+          ))}
+        </div>
+      </ErrorBoundary>
+    </section>
   );
 }
