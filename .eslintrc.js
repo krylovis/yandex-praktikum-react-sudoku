47a51a4895e2000aa4452a2486b2325f868e676a
nikitayakovina <36497484+nikitayakovina@users.noreply.github.com>
2025-04-12 21:06:22 +0600

    TASK-22: rollback

--- .eslintrc.js
+++ .eslintrc.js
@@ -66,7 +66,6 @@ module.exports = {
     'object-curly-newline': 'off',
     'jsx-a11y/label-has-associated-control': 'off',
     'jsx-a11y/label-has-for': 'off',
-    'react/require-default-props': 'off',
-    'operator-linebreak': 'off'
+    'react/require-default-props': 'off'
   },
 };
